22dd84e4f8b882b857a407dabfefb5fa741bdd38
FallWaterFall <volsoor@gmail.com>
2022-02-17 14:49:52 +0200

    Lab1 fix

--- Lab1/Assets/PlayerMovement.cs
+++ Lab1/Assets/PlayerMovement.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cat : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
 {
-    [Header("Player velocity")]
-    //Ox
-    public int xVelocity = 5;
-    // Oy
-    public int yVelocity = 8;
+    [Header("Player velocity controll")]
+    [SerializeField] private int xVelocity = 5;
+    [SerializeField] private int yVelocity = 8;
+    [Header("Player jump controll")]
+    [SerializeField] private float timeToJump = 0.0f;
+    [SerializeField] private float jumpDeltaTime = 1.5f;
 
     //
     private Rigidbody2D rigidBody;
@@ -31,7 +33,6 @@ public class Cat : MonoBehaviour
         float moveInput = Input.GetAxis("Horizontal");
         float jumpInput = Input.GetAxis("Jump");
 
-
         if (moveInput < 0)
         {
             rigidBody.velocity = new Vector2(-xVelocity, rigidBody.velocity.y);
@@ -41,8 +42,10 @@ public class Cat : MonoBehaviour
             rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
         }
 
-        if (jumpInput > 0)
+        if (timeToJump > 0) timeToJump -= Time.deltaTime;
+        if (jumpInput > 0 && timeToJump <= 0)
         {
+            timeToJump = jumpDeltaTime;
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, yVelocity);
         }
     }
