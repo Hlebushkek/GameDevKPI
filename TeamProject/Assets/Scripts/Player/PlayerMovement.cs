a24155568d207e169d388eb6faf5bb0b53cad639
Hlib Sobolevskyi <volsoor@gmail.com>
2022-06-16 02:17:11 +0300

    Player Rotation

--- TeamProject/Assets/Scripts/Player/PlayerMovement.cs
+++ TeamProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Camera cam;
+
     private Vector2 movementDirection;
     private float movementSpeed;
     [SerializeField] private float MOVEMENT_BASE_SPEED = 8f;
     private bool canMove = true;
     private bool isMoving = false;
+
+    private Vector2 mousePosition;
     private void Awake()
     {
         rb = GetComponentInParent<Rigidbody2D>();
+        cam = FindObjectOfType<Camera>();
     }
     private void Update()
     {
         ProcessInputs();
 
-        if (canMove)
-        {
-            Move();
-        }
+        TryMove();
+
+        TryRotate();
     }
 
     private void ProcessInputs()
@@ -30,12 +34,25 @@ public class PlayerMovement : MonoBehaviour
         movementDirection.Normalize();
 
         if (movementDirection.x != 0 || movementDirection.y != 0) { isMoving = true; }
-        else { isMoving = false;}
+        else { isMoving = false; }
 
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
     }
-    private void Move()
+
+    private void TryMove()
     {
-        rb.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
+        if (canMove)
+        {
+            rb.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
+        } else { rb.velocity = Vector2.zero; }
+        
+    }
+
+    private void TryRotate()
+    {
+        Vector2 lookDir = mousePosition - rb.position;
+        float angle =  Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
     }
 
     public void SetMoving(bool canMove)
