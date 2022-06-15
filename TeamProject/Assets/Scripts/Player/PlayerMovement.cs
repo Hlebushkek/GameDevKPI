5df3961a2306ac82e66d363a9e1e52cf21b8ed99
Hlib Sobolevskyi <volsoor@gmail.com>
2022-06-16 01:32:11 +0300

    Player Movement Fix

--- TeamProject/Assets/Scripts/Player/PlayerMovement.cs
+++ TeamProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,24 +1,41 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+    private float movementSpeed;
+    [SerializeField] private float MOVEMENT_BASE_SPEED = 8f;
     private bool canMove = true;
     private bool isMoving = false;
-    [SerializeField] private float verticalSpeed = 10f;
-    [SerializeField] private float horizonatalSpeed = 10f;
-    private float verticalMove, horizontalMove;
+    private void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+    }
     private void Update()
     {
-        if (canMove) {
-            verticalMove = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
-            horizontalMove = Input.GetAxis("Horizontal") * horizonatalSpeed * Time.deltaTime;
-            
-            if (verticalMove != 0 || horizontalMove != 0) { isMoving = true;}
-            else {isMoving = false;}
+        ProcessInputs();
 
-            transform.Translate(0, verticalMove, 0);
-            transform.Translate(horizontalMove, 0, 0);
+        if (canMove)
+        {
+            Move();
         }
+    }
+
+    private void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+
+        if (movementDirection.x != 0 || movementDirection.y != 0) { isMoving = true; }
+        else { isMoving = false;}
+
+    }
+    private void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
     }
 
     public void SetMoving(bool canMove)
